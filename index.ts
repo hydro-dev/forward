40bee648d5bd8c254da4f79e16667a046f6eb81d
undefined <i@undefined.moe>
2023-07-12 01:24:52 +0800

    watch alias changes

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ resolver.setServers(['114.114.114.114', '8.8.8.8']);
 const logger = new Logger('proxy');
 const fails = Counter();
 function createProxy(target: string, targetPort: string) {
+    let stop = false;
     logger.info('Proxy to', target);
     const child = exec(`ssh -R 0.0.0.0:${targetPort}:${local} root@${target}.hydro.ac`);
     child.stderr!.on('data', (data) => {
@@ -37,19 +38,46 @@ function createProxy(target: string, targetPort: string) {
         }
     }, 60000);
     child.on('exit', (code, signal) => {
+        if (stop) return;
         if (fails[target] % 10 === 9) global.sendMessage?.(`Proxy to ${target} failed after 10 retries.`);
         fails[target]++;
         logger.warn(`Proxy process to ${target} exited with code ${code}, signal ${signal}`);
         clearInterval(interval);
         setTimeout(() => createProxy(target, targetPort), 1000);
     });
+    return () => {
+        stop = true;
+        child.kill();
+    }
 }
 
 async function main() {
+    const map = {};
     const [[result]] = await resolver.resolveTxt('alias.hydro.ac');
-    const aliases = result.split(' ').filter(i => i);
+    let aliases = result.split(' ').filter(i => i);
     console.log(aliases);
-    for (const r of aliases) createProxy(r, process.argv[2]);
+    for (const r of aliases) map[r] = createProxy(r, process.argv[2]);
+    setInterval(async () => {
+        try {
+            const [[result]] = await resolver.resolveTxt('alias.hydro.ac');
+            const newAliases = result.split(' ').filter(i => i);
+            console.log(newAliases);
+            for (const r of newAliases) {
+                if (map[r]) continue;
+                map[r] = createProxy(r, process.argv[2]);
+                logger.info('New alias', r);
+            }
+            for (const r of aliases) {
+                if (newAliases.includes(r)) continue;
+                map[r]();
+                delete map[r];
+                logger.info('Alias removed', r);
+            }
+            aliases = newAliases;
+        } catch (e) {
+            logger.warn('Failed to resolve alias.hydro.ac');
+        }
+    }, 60000);
 }
 if (!process.argv[2]) logger.error('port required');
 else main();
