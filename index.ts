4868d36ebbd4c87d65b328db7718549d56ee64ba
undefined <i@undefined.moe>
2023-11-17 19:06:01 +0000

    allow multiple ports

--- index.ts
+++ index.ts
@@ -2,18 +2,18 @@ import { exec } from 'child_process';
 import { Resolver } from 'dns/promises';
 import { Counter, Logger } from '@hydrooj/utils';
 
-const local = '192.168.99.1:2333';
 const resolver = new Resolver();
 resolver.setServers(['114.114.114.114', '8.8.8.8']);
 const logger = new Logger('proxy');
 const fails = Counter();
-function createProxy(target: string, targetPort: string) {
+function createProxy(target: string, targetPort: string, local: string) {
+    const identifer = target + ':' + targetPort;
     let stop = false;
-    logger.info('Proxy to', target);
-    const child = exec(`ssh -R 0.0.0.0:${targetPort}:${local} root@${target}.hydro.ac`);
+    logger.info('Proxy to', identifer);
+    const child = exec(`ssh -R 127.0.0.1:${targetPort}:${local} root@${target}.hydro.ac`);
     child.stderr!.on('data', (data) => {
         if (!data.toString().includes('remote port forwarding failed for listen port')) return;
-        logger.info(`Kill old ssh process on ${target}`);
+        logger.info(`Kill old ssh process on ${identifer}`);
         child.stdin!.write(`fuser -k ${targetPort}/tcp\n`);
     });
     let token = '';
@@ -21,8 +21,8 @@ function createProxy(target: string, targetPort: string) {
         const s = data.toString();
         if (s.includes(token)) token = '';
         if (s.includes('Welcome')) {
-            fails[target] = 0;
-            logger.success(`Connected to ${target}`);
+            fails[identifer] = 0;
+            logger.success(`Connected to ${identifer}`);
         }
         if (s.includes('fuser') && s.includes('command not found')) {
             logger.info(`Installing fuser on ${target}`);
@@ -39,15 +39,23 @@ function createProxy(target: string, targetPort: string) {
     }, 60000);
     child.on('exit', (code, signal) => {
         if (stop) return;
-        if (fails[target] % 10 === 9) global.sendMessage?.(`Proxy to ${target} failed after 10 retries.`);
-        fails[target]++;
-        logger.warn(`Proxy process to ${target} exited with code ${code}, signal ${signal}`);
+        if (fails[identifer] % 10 === 9) global.sendMessage?.(`Proxy to ${identifer} failed after 10 retries.`);
+        fails[identifer]++;
+        logger.warn(`Proxy process to ${identifer} exited with code ${code}, signal ${signal}`);
         clearInterval(interval);
-        setTimeout(() => createProxy(target, targetPort), 1000);
+        setTimeout(() => createProxy(target, targetPort, local), 1000);
     });
     return () => {
         stop = true;
         child.kill();
+    }
+}
+
+function initAllProxy(target: string) {
+    const tasks = process.argv[2].split(',');
+    for (const tasks of tasks) {
+        const [l, r] = task.split('->');
+        createProxy(target, l, r);
     }
 }
 
@@ -56,7 +64,7 @@ async function main() {
     const [[result]] = await resolver.resolveTxt('alias.hydro.ac');
     let aliases = result.split(' ').filter(i => i);
     console.log(aliases);
-    for (const r of aliases) map[r] = createProxy(r, process.argv[2]);
+    for (const r of aliases) map[r] = initAllProxy(r);
     setInterval(async () => {
         try {
             const [[result]] = await resolver.resolveTxt('alias.hydro.ac');
@@ -64,7 +72,7 @@ async function main() {
             console.log(newAliases);
             for (const r of newAliases) {
                 if (map[r]) continue;
-                map[r] = createProxy(r, process.argv[2]);
+                map[r] = initAllProxy(r);
                 logger.info('New alias', r);
             }
             for (const r of aliases) {
@@ -79,5 +87,5 @@ async function main() {
         }
     }, 60000);
 }
-if (!process.argv[2]) logger.error('port required');
+if (!process.argv[2]) logger.error('Usage: forward 2333->192.168.1.1:2333,2334->192.168.1.1:2444 (remote->local)');
 else main();
