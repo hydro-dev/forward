ccc92d6c74fc33acda96d1033664026507bd6fc7
undefined <i@undefined.moe>
2024-03-06 11:32:55 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import { exec } from 'child_process';
+import { ChildProcess, exec } from 'child_process';
 import { Resolver } from 'dns/promises';
 import { Counter, fs, Logger } from '@hydrooj/utils';
 
 const resolver = new Resolver();
 resolver.setServers(['114.114.114.114', '8.8.8.8']);
 const logger = new Logger('proxy');
+const childrens: ChildProcess[] = [];
 const fails = Counter();
 function createProxy(target: string, targetPort: string, local: string) {
     const identifer = target + ':' + targetPort;
     let stop = false;
     logger.info('Proxy to', identifer);
     const child = exec(`ssh -tt -R 127.0.0.1:${targetPort}:${local} root@${target}.hydro.ac`);
+    childrens.push(child);
 
     let token = '';
     function onLineReceived(data: string) {
@@ -45,6 +47,7 @@ function createProxy(target: string, targetPort: string, local: string) {
         if (stop) return;
         if (fails[identifer] % 10 === 9) global.sendMessage?.(`Proxy to ${identifer} failed after 10 retries.`);
         fails[identifer]++;
+        childrens.splice(childrens.indexOf(child), 1);
         logger.warn(`Proxy process to ${identifer} exited with code ${code}, signal ${signal}`);
         clearInterval(interval);
         setTimeout(() => createProxy(target, targetPort, local), 1000);
@@ -100,3 +103,8 @@ async function main(expr: string) {
 const alt = fs.existsSync('config') ? fs.readFileSync('config', 'utf-8') : '';
 if (!process.argv[2] && !alt) logger.error("Usage: forward '2333->192.168.1.1:2333,2334->192.168.1.1:2444' (remote->local)");
 else main(process.argv[2] || alt);
+
+process.on('SIGINT', () => {
+    for (const child of childrens) child.kill();
+    process.exit();
+});
